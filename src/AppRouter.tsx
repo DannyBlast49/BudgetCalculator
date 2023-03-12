6ad4ccee887783e34dfd34cfe82df6a75d45ea9a
danjimenez123 <danjimenez123@gmail.com>
2023-03-12 16:13:24 -0700

    Installed scss and yarn. Modified Navigation cmp

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -4,13 +4,32 @@ import Calculator from './Calculator/Calculator';
 import Expenses from './Expenses/Expenses';
 import Savings from './Savings/Savings';
 import Settings from './Settings/Settings';
+import PageNotFound from './PageNotFound';
+import { navList } from './Navigation/constants';
 
 const AppRouter = () => (
   <Routes>
-    <Route path="/" element={<Calculator />} />
-    <Route path="/Expenses" element={<Expenses />} />
-    <Route path="/Savings" element={<Savings />} />
-    <Route path="/Settings" element={<Settings />} />
+    { 
+      navList.map((link) => {
+        switch (link.name) {
+          case 'calculator': {
+            return (<Route path={link.path} element={<Calculator />} />);
+          }
+          case 'expenses': {
+            return (<Route path={link.path} element={<Expenses />} />);
+          }
+          case 'savings': {
+            return (<Route path={link.path} element={<Savings />} />);
+          }
+          case 'settings': {
+            return (<Route path={link.path} element={<Settings />} />);
+          }
+          default: {
+            return (<Route path="/PageNotFound" element={<PageNotFound />} />);
+          }
+        }
+      })
+    }
   </Routes>
 );
 
---
+++ src/Navigation/types.ts
@@ -0,0 +1,7 @@
+import { ReactJSXElement } from "@emotion/react/types/jsx-namespace";
+
+export type NavigationLink = {
+  name: string;
+  label: string;
+  path: string;
+}
