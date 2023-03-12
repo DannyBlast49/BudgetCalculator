b85d84513545bc44e29716439fbd7875a068636d
danjimenez123 <danjimenez123@gmail.com>
2023-03-11 21:39:56 -0800

    Added Material UI

--- src/Navigation/Navigation.tsx
+++ src/Navigation/Navigation.tsx
@@ -1,21 +1,38 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import Button from '@mui/material/Button';
 
 const Navigation = () => (
   <div>
     <nav>
       <ul>
         <li>
-          <Link to="/">Calculator</Link>
+          <Link className="nav-button" to="/">
+            <Button variant="contained">
+              Calculator
+            </Button>
+          </Link>
         </li>
         <li>
-          <Link to="/Expenses">Expenses</Link>
+          <Link className="nav-button" to="/Expenses">
+            <Button variant="contained">
+              Expenses
+            </Button>
+          </Link>
         </li>
         <li>
-          <Link to="/Savings">Savings</Link>
+          <Link className="nav-button" to="/Savings">
+            <Button variant="contained">
+              Savings
+            </Button>
+          </Link>
         </li>
         <li>
-          <Link to="/Settings">Settings</Link>
+          <Link className="nav-button" to="/Settings">
+            <Button variant="contained">
+              Settings
+            </Button>
+          </Link>
         </li>
       </ul>
     </nav>
